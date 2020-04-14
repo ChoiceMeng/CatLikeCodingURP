610e89532b26e93f70fec32b57f6cd39340577b0
choice <winmain@vip.qq.com>
2020-04-14 19:44:45 +0800

    第一章代码

---
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+    CullingResults cullingResults;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera);
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId, sortingSettings
+        );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+
+        Debug.Log("DrawVisibleGeometry");
+        context.DrawSkybox(camera);
+    }
+
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+        //context.SetupCameraProperties(camera);
+    }
+
+    // 提交
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        context.Submit();
+    }
+
+    // 拷贝
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        //ScriptableCullingParameters p
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
