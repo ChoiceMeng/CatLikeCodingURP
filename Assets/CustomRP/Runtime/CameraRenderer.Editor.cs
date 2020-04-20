bf227b40a0f5b12a97e6715e12a27be67d1a52fc
winmain <winmain@vip.qq.com>
2020-04-20 22:44:01 +0800

    第一章

---
+++ Assets/CustomRP/Runtime/CameraRenderer.Editor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+partial class CameraRenderer
+{
+
+    partial void DrawGizmos();
+    partial void PrepareBuffer();
+
+#if UNITY_EDITOR
+
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
+
+	string SampleName { get; set; }
+
+	partial void PrepareBuffer()
+    {
+        buffer.name = SampleName = camera.name;
+    }
+
+#else
+
+	const string SampleName = bufferName;
+#endif
+}
--- Assets/CustomRP/Runtime/CameraRenderer.cs
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
 
-public class CameraRenderer
+public partial class CameraRenderer
 {
     ScriptableRenderContext context;
 
@@ -13,6 +13,7 @@ public class CameraRenderer
         this.context = context;
         this.camera = camera;
 
+        PrepareBuffer();
         if (!Cull())
         {
             return;
@@ -20,24 +21,66 @@ public class CameraRenderer
 
         Setup();
         DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
     }
 
     static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+    static ShaderTagId[] legacyShaderTagIds = {
+        new ShaderTagId("Always"),
+        new ShaderTagId("ForwardBase"),
+        new ShaderTagId("PrepassBase"),
+        new ShaderTagId("Vertex"),
+        new ShaderTagId("VertexLMRGBM"),
+        new ShaderTagId("VertexLM")
+    };
     void DrawVisibleGeometry()
     {
-        var sortingSettings = new SortingSettings(camera);
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
         var drawingSettings = new DrawingSettings(
             unlitShaderTagId, sortingSettings
         );
-        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+        // 非透明物体
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+        // 天空盒
+        context.DrawSkybox(camera);
+        // 透明物体
+        filteringSettings = new FilteringSettings(RenderQueueRange.transparent);
+        context.DrawRenderers(
+            cullingResults, ref drawingSettings, ref filteringSettings
+        );
+    }
 
+    static Material errorMaterial;
+    void DrawUnsupportedShaders()
+    {
+        var drawingSettings = new DrawingSettings(
+            legacyShaderTagIds[0], new SortingSettings(camera)
+        )
+        {
+            overrideMaterial = errorMaterial
+        };
+        for (int i = 1; i < legacyShaderTagIds.Length; i++)
+        {
+            drawingSettings.SetShaderPassName(i, legacyShaderTagIds[i]);
+        }
+        var filteringSettings = FilteringSettings.defaultValue;
         context.DrawRenderers(
             cullingResults, ref drawingSettings, ref filteringSettings
         );
 
-        Debug.Log("DrawVisibleGeometry");
-        context.DrawSkybox(camera);
+        if (errorMaterial == null)
+        {
+            errorMaterial =
+                new Material(Shader.Find("Hidden/InternalErrorShader"));
+        }
     }
 
     const string bufferName = "Render Camera";
@@ -50,10 +93,15 @@ public class CameraRenderer
     void Setup()
     {
         context.SetupCameraProperties(camera);
-        buffer.ClearRenderTarget(true, true, Color.clear);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags == CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear
+        );
         buffer.BeginSample(bufferName);
         ExecuteBuffer();
-        //context.SetupCameraProperties(camera);
     }
 
     // 提交
@@ -66,8 +114,10 @@ public class CameraRenderer
     // 拷贝
     void ExecuteBuffer()
     {
+        buffer.BeginSample(bufferName);
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
+        buffer.EndSample(bufferName);
     }
 
     bool Cull()
